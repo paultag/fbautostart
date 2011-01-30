79eb85ccc68c95d23c940bd84d70563bc59c062a
Paul Tagliamonte <paultag@ubuntu.com>
2011-01-29 23:13:57 -0500

    stupid longer then 80 char crap

--- src/fbautostart.cpp
+++ src/fbautostart.cpp
@@ -78,7 +78,9 @@ int runCommand( std::string appl ) {
 		return pid;
 
 	// get shell path from the environment
-	// this process exits immediately, so we don't have to worry about memleaks
+	//
+	// this process exits immediately, so we don't have to
+	//  worry about memleaks
 	std::string shell = getenv("SHELL");
 
 	if ( shell != "" )
